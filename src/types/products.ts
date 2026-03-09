a446d7f693a17390832b057ef75237ed3131193c
oscar-abud <oscarpalma.vpr2@gmail.com>
2026-03-09 12:08:27 -0300

    [FEAT] Add the type of 'Producto extra' and 'ProductExtraClient'

--- src/types/products.ts
+++ src/types/products.ts
@@ -5,11 +5,27 @@ export interface Product {
   cantidad: number;
 }
 
+export interface ProductExtra {
+  _id: string;
+  name: string;
+  type: string;
+  quantity: number;
+  price: number;
+}
+
 export interface ProductClient {
   _id: string;
   name: string;
   productos: Product[];
   price: number;
   state: boolean;
+  __v?: number;
+}
+
+export interface ProductExtraClient {
+  _id: string;
+  name: string;
+  description?: string;
+  productosExtras: ProductExtra[];
   __v?: number;
 }
