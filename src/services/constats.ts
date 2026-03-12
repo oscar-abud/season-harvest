13f43591529539d0f778dd48579b552c539b6daa
oscar-abud <oscarpalma.vpr2@gmail.com>
2026-03-12 17:26:20 -0300

    HOTFIX

---
+++ src/services/constats.ts
@@ -0,0 +1,9 @@
+const apiPrefix = 'api'
+// MOSTRAR LOS PRODUCTOS DE LOS CLIENTES
+export const CONST_ENDPOINT_PRODUCTOS_CLIENTES = `${apiPrefix}/productos-clientes`;
+// MOSTRAR PRODUCTOS EXTRAS A CLIENTES
+export const CONST_ENDPOINT_PRODUCTOS_EXTRAS = `${apiPrefix}/productos-extras`;
+// USUARIOS
+export const CONST_ENDPOINT_USUARIOS = `${apiPrefix}/usuario`;
+// CONTACTOS
+export const CONST_ENDPOINT_CONTACTOS = `${apiPrefix}/contacto`;
